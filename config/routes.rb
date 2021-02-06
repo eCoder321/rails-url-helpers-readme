f0abe8c133331c6f1c1e58e30c405c907f828add
ecoder321 <femiwonderful@gmail.com>
2021-02-06 13:22:02 -0500

    passing tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #resources :posts, only: :new, as: :register # make this route new_register_path
+  get '/users/new', to: 'users#new', as: 'register' #renames this path as register_path
   resources :posts, only: [:index, :show]
+  #resources :posts, only: :new 
 end
